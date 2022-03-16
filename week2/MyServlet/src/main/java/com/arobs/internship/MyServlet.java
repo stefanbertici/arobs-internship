5ca56d798484df6c89ada82ce2a0b42f14e9944f
Stefan Bertici <69802287+stefanbertici@users.noreply.github.com>
2022-03-16 16:03:25 +0200

    servlet update

--- week2/MyServlet/src/main/java/com/arobs/internship/MyServlet.java
+++ week2/MyServlet/src/main/java/com/arobs/internship/MyServlet.java
@@ -1,6 +1,5 @@
 package com.arobs.internship;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,11 +14,15 @@ public class MyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter out = resp.getWriter();
-        out.println("<h1>Hello from GET! Please log in.</h1>");
-        out.println("<form action=\"./login\" method=POST>" +
-                    "Name: <input type=\"text\" name=\"name\"> <br/>" +
-                    "Password#: <input type=\"text\" name=\"password\" />" +
-                    "<input type=\"submit\" value=\"Submit\" /> </form>");
+        out.println("""
+                    <h1>Hello from GET! Please log in.</h1>
+                    
+                    <form name="loginForm" action="./login" method="POST">
+                        Name: <input type="text" name="name"/> <br/>
+                        Password: <input type="text" name="password"/> <br/>
+                        <input type="submit" value="Submit"/>
+                    </form>
+                    """);
     }
 
     @Override
