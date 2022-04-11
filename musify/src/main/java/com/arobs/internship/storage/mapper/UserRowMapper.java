cb6b64f3dfb1cea35fb8af0ed2a291980e17198e
Stefan Bertici <69802287+stefanbertici@users.noreply.github.com>
2022-04-11 15:19:18 +0300

    moved replaced classes into storage package

---
+++ musify/src/main/java/com/arobs/internship/storage/mapper/UserRowMapper.java
@@ -0,0 +1,16 @@
+package com.arobs.internship.storage.mapper;
+
+import com.arobs.internship.musify.model.User;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserRowMapper implements RowMapper<User> {
+    @Override
+    public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return new User(rs.getInt("id"), rs.getString("first_name"), rs.getString("last_name"),
+                rs.getString("email"), rs.getString("encrypted_password"), rs.getString("country"),
+                rs.getString("role"), rs.getString("status"));
+    }
+}
